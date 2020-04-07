cc54fceafc57ace9447ef5cb5728530106af5b36
Gabe Wright <gabe1331@gmail.com>
2020-04-07 16:18:30 -0400

    firt commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/* _______________________________________________________________
+Part 1.) It's the UX bunch! 
+
+To reveal who is behind boxes 2 and 3, we'll need  to use some 
+DOM methods and change their opacity back to 1. 
+
+We've assigned ID's of "box-two" and "box-nine" to  the remaining 
+boxes. They currently have the opacity set to 0.  Using a DOM 
+selector and the style property, write pure JavaScript to reveal 
+who's behind the remaining boxes. 
+________________________________________________________________*/
+
+// Your code here
+
+
+
+
+
+/*________________________________________________________________
+Part 2.) The Hidden Message
+
+Lastly, we've added an ID of "hidden-message" to an empty paragrah 
+element. Use JavaScript to inject the paragragh's inner HTML with a 
+string containing whatever text you want.
+________________________________________________________________*/
+
+// Your code here 
+
+
+
+
